6e86817b16a8bcfe5a1c16abf5ea3f8b62be554a
AluminiumTech <14107316+AluminiumTech@users.noreply.github.com>
2020-12-18 12:08:52 +0000

    add Avalonia project so that it can be worked on in the future

---
+++ AvaloniaApp/OpenFrafsuallyDesktop.Avalonia/MainWindow.axaml.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace OpenFrafsuallyDesktop.Avalonia
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+           
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
